e295c01d3ab73dea80170319bd022a405efb80d5
Kumar Shanu <shanuK@aarth.io>
2022-08-15 20:49:11 +0530

    mail send using app

---
+++ Notification/sendingEmail.js
@@ -0,0 +1,37 @@
+/**
+ * this file will contain the sample logic to send the email
+ */
+
+const nodemailer = require('nodemailer');
+
+/**
+ * we need to configure the transporter for sending email
+ *
+ */
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  auth: {
+    user: 'krksingh.99@gmail.com',
+    pass: 'kkqaairsldnjxyxv',
+  },
+  secure: true,
+});
+
+/**
+ * write the code to send the email
+ *
+ */
+const mailObj = {
+  from: 'crm-no-reply@gmail.com',
+  to: 'krshanu.95@gmail.com',
+  subject: 'Testing code - for  sending email',
+  text: 'Sample text content of the email',
+};
+transporter.sendMail(mailObj, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Email was send successfully');
+  }
+});
